eafcfb41f75a52135a17356908670630e640a49f
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-16 13:00:23 +0100

    complete snack 1

---
+++ src/org/lessons/java/snacks/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.snacks;
+
+public class Main {
+    public static void main(String[] args) {
+        Studente Maks = new Studente("Maks", "Bernatskyy", 19);
+
+        Maks.getAllInformations();
+    }
+}
--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -1,5 +1,53 @@
 package org.lessons.java.snacks;
 
 public class Studente {
+    // Attributi
+    private String nome;
+    private String cognome;
+    private int età;
 
+    // Metodi
+
+    public Studente(String nome, String cognome, int età) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.equals("")) {
+            this.nome = nome;
+        }
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome) {
+        if (cognome != null && !cognome.equals("")) {
+            this.cognome = cognome;
+        }
+    }
+
+    public int getEtà() {
+        return this.età;
+    }
+
+    public void setEtà(int età) {
+        if (età > 0 && età < 120) {
+            this.età = età;
+        }
+    }
+
+    public void getAllInformations() {
+        System.out.printf("Studente: %s %s, %s anni",
+                this.nome,
+                this.cognome,
+                this.età);
+    }
 }
